bf8fa1a1e7f66a04fc2a687a0ae54354b2e58cc9
Kristina <kristin.petrivska@gmail.com>
2024-07-17 18:08:41 -0600

    add solution

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+'use strict';
 
+let result = [];
+const n = prompt('Enter number N.');
+if ( n === null){
+    alert('You canceled the input.');
+} else if ( !n.trim() ){
+    alert('Your input is empty.');
+} else if ( isNaN(n) ){
+    alert('Input must be a number.');
+} else if ( !Number.isInteger(n) ){
+    alert('Must be an integer number.') ;
+} else {
+    const smallestNum = 1;
+    const biggerstNum = 100;
+    for ( let i = smallestNum; i <= biggerstNum ; i++ ){
+        if (i ** 2 < n){
+        result.push(i);
+        }
+    }
+
+    console.log(result);
+    }
